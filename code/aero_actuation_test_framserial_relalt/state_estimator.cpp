35df493b3da9bbc20957a18224591be8cd62db75
ebregin <71725639+ebregin@users.noreply.github.com>
2022-05-29 08:46:38 -0400

    Update state_estimator.cpp

--- code/aero_actuation_test_framserial_relalt/state_estimator.cpp
+++ code/aero_actuation_test_framserial_relalt/state_estimator.cpp
@@ -9,6 +9,9 @@ using namespace BLA;
   Matrix<3,3> F_k={0};
   
   Matrix<3> G_k={0};
+
+  Matrix<1,3> H_k = {0};
+
 float sigmaA=0.1f;
 float sigmaB=0.0001f;
 float Ts =0.1f;
@@ -32,14 +35,34 @@ float velocity_filter = 0.0f;
   int n = 3;
   int p = 1;
   float R=0.0001f;
+   float height_k = 0.0f;  
+  float acc_k = 0.0f; //it is absolute acceleration! (not relative!)
+  float theta_k =0.0f; //in rad
+
+  float u_k=0;
+
+  Matrix<3> xk_minus =  {0};               
+Matrix<3,3> Pk_minus ={0};
+
+Matrix<3,1>H_k_T={0};
+float S_k = 0;
+Matrix<3> K_k = {0};
+
+// weight to previous estimates
+Matrix<3,3> I3 = {0};
+Matrix<3,3> W_k={0};
+
+//update step (posterior)
+Matrix<3>x_k = {0};
+Matrix<3,3>P_k = {0};
   
 
 void updateState(){
 
     // Get sensor data
-  float height_k = getRelAltitude();  
-  float acc_k = getRelAccelX(); //it is absolute acceleration! (not relative!)
-  float theta_k =0.0f; //in rad
+  height_k = getRelAltitude();  
+  acc_k = getRelAccelX(); //it is absolute acceleration! (not relative!)
+  theta_k =0.0f; //in rad
   
 // Create system matricies
    F_k = 
@@ -50,28 +73,28 @@ void updateState(){
 
  G_k = { 0.5*Ts*Ts    ,  Ts  , 0    };
 
-Matrix<1,3> H_k = { 1    ,  0  , 0    };
+ H_k = { 1    ,  0  , 0    };
 
 // input from IMU for process prediction
-float u_k= -acc_k - g*cos(theta_k);
+u_k= -acc_k - g*cos(theta_k);
 
 //prediction step (prior)
-Matrix<3> xk_minus =  F_k*previousState + G_k*u_k ;               
-Matrix<3,3> Pk_minus = F_k * previousVariance *(~F_k)+Q;
+xk_minus =  F_k*previousState + G_k*u_k ;               
+Pk_minus = F_k * previousVariance *(~F_k)+Q;
 
 // Kalman gain
-Matrix<3,1>H_k_T=~H_k;
-//Matrix<1> S_k = (H_k * Pk_minus * H_k_T)+R;
-float S_k = Pk_minus(1,1)+R;
-Matrix<3> K_k = Pk_minus * H_k_T/S_k;
+H_k_T=~H_k;
+
+S_k = Pk_minus(1,1)+R;
+K_k = Pk_minus * H_k_T/S_k;
 
 // weight to previous estimates
-Matrix<3,3> I3 = {1,0,0,0,1,0,0,0,1};
-Matrix<3,3> W_k=I3-K_k*H_k;
+ I3 = {1,0,0,0,1,0,0,0,1};
+ W_k=I3-K_k*H_k;
 
 //update step (posterior)
-Matrix<3>x_k = W_k*xk_minus+K_k*height_k;
-Matrix<3,3>P_k = W_k*Pk_minus*(~W_k)+K_k*R*(~K_k);
+x_k = W_k*xk_minus+K_k*height_k;
+P_k = W_k*Pk_minus*(~W_k)+K_k*R*(~K_k);
 
 height_filter=x_k(1);
 velocity_filter=x_k(2);
